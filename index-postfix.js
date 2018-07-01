e55dd8900056c7fe1f2562bfdc84e3228589e880
Michael Tarleton <michael.tarleton@me.com>
2018-07-01 00:01:45 -0500

    : Init

---
+++ index-postfix.js
@@ -0,0 +1,18 @@
+const { fork } = require('child_process');
+const http = require('http');
+const server = http.createServer();
+
+server.on('request', (req,res) => {
+    if(req.url === '/compute') {
+        const compute = fork('long-computer-sample-fork.js');
+        compute.send('start');
+        compute.on('message', sum => {
+            res.end(`Sum is ${sum}`);
+        });
+    } else {
+        res.end('Ok');
+    }
+});
+
+server.listen(3000, () => console.log('Listening on port 3000'));
+
