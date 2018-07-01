cab5243eb23009fd6f499e241363235df7d18af2
Michael Tarleton <michael.tarleton@me.com>
2018-07-01 00:08:18 -0500

    master: More stuff

--- index-postfix.js
+++ index-postfix.js
@@ -15,4 +15,3 @@ server.on('request', (req,res) => {
 });
 
 server.listen(3000, () => console.log('Listening on port 3000'));
-
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const { fork } = require('child_process');
 const http = require('http');
-const server = http.createServer();
 
+const longComputation = () => {
+    let sum = 0;
+    for(let i = 0; i < 1e9; i++) {
+        sum += 1;
+    }
+    return sum;
+}
+
+const server = http.createServer();
 server.on('request', (req,res) => {
     if(req.url === '/compute') {
-        const compute = fork('long-computer-sample-fork.js');
-        compute.send('start');
-        compute.on('message', sum => {
-            res.end(`Sum is ${sum}`);
-        });
+        const compute = longComputation();
+        res.end(`Sum is ${sum}`);
     } else {
         res.end('Ok');
     }
 });
 
 server.listen(3000, () => console.log('Listening on port 3000'));
-
